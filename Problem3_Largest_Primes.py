6c5859c5a91b89f4b6668040b6bd1f18c80b3985
Sharon Ruane <sharon.ruane@gmail.com>
2019-06-10 10:39:38 +0100

    first commit baby problems

---
+++ Problem3_Largest_Primes.py
@@ -0,0 +1,9 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# # What is the largest prime factor of the number 600851475143 ?
+
+def largest_prime(n):
+
+    # need to check if it is a prime  ... how
+    # need to work out if it's the largest ... list til hit the target seems very inefficient
+
+
